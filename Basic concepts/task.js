29b4209de3d3f42f779c11733dbbe7286e550e83
Denis Khusainov <denis.khusainov.2017@mail.ru>
2023-03-19 16:54:58 +0300

    Add task for basic concepts

---
+++ Basic concepts/task.js
@@ -0,0 +1,26 @@
+"use strict"
+function solveEquation(a, b, c){
+    let d = b**2-4*a*c;
+    let result = 0;
+    let x1 = 0;
+    let x2 = 0;
+    if(d < 0){
+        return result = ["нет корней"];
+    }
+    if(d === 0){
+        x1 = -b/(2*a);
+        return result = [x1];
+    }
+    if(d > 0){
+        x1 = (-b + Math.sqrt(d) )/(2*a);
+        x2 = (-b - Math.sqrt(d) )/(2*a);
+        return result = [x1, x2]
+    }
+}
+function calculateTotalMortgage(percent, contribution, amount, countMonths){
+    let P = (percent / 100) / 12;
+    let S = amount - contribution;
+    let paymentInMonths = (S * (P + (P / (((1 + P)** countMonths) - 1))));
+    let totalPayment = Number((paymentInMonths * countMonths).toFixed(2));
+    return totalPayment
+}
